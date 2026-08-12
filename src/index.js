33a449f0b4671996e7397a17992fde7aeaa9e664
igor-tosi <igorvenanciotosi@gmail.com>
2026-08-11 21:44:33 -0300

    criacao de uns bgl do script.js

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 const express = require("express");
-const rotas = require("./routes");
 const path = require("path");
+const rotas = require("./routes");
 const { conectarBancoDeDados } = require("./db/connection");
 const { middlewareDeAutenticacao } = require("./middlewares");
 
@@ -10,18 +10,18 @@ conectarBancoDeDados();
 
 app.use(express.json());
 
-app.use(express.static(path.join(__dirname, "views")));
+app.use(express.static(path.join(__dirname, "views")))
 
 app.use((req, res, next) => {
     console.log(`${req.method} ${req.url} - ${new Date().toISOString()}`);
     next();
 });
 
-//app.use(middlewareDeAutenticacao);
-
-app.get("/", (req,res) => {
-    res.sendFile(path.join(__dirname, "views", "index.html"))
+app.get("/", (req, res) => {
+    return res.sendFile(path.join(__dirname, "views", "index.html"));
 })
+
+// app.use(middlewareDeAutenticacao);
 
 app.use("/", rotas);
 
--- src/views/script.js
+++ src/views/script.js
@@ -1,18 +1,68 @@
 const formularioTarefa = document.getElementById("formulario-tarefa");
 const listaTarefas = document.getElementById("lista-tarefas");
+
 const campoTitulo = document.getElementById("titulo-tarefa");
 const campoDescricao = document.getElementById("descricao-tarefa");
 const campoIdTarefa = document.getElementById("id-tarefa");
 const botaoCancelar = document.getElementById("botao-cancelar");
 const botaoEnviar = document.getElementById("botao-enviar");
 
+async function buscarTarefas() {
+    const response = await fetch("/tarefas");
 
-async function buscarTarefas(){
-    const response = await fetch ("/tarefas");
-    
     if (response.ok) {
         const dados = await response.json();
-        console.log(dados)
+        return dados;
     }
-};
-buscarTarefas()
+    return [];
+}
+
+async function listarTarefas() {
+    const dados = await buscarTarefas();
+    const tarefas = dados.tarefas;
+
+    listaTarefas.innerHTML = "";
+
+    tarefas.forEach(tarefa => {
+        const item = document.createElement("li");
+
+        item.classList.add("item-tarefa");
+
+        item.innerHTML = `
+            <div class="conteudo-tarefa">
+                <div>
+                    <h3 class="titulo-tarefa">${tarefa.titulo}</h3>
+                    <p class="descricao-tarefa">${tarefa.descricao}</p>
+                </div>
+            </div>
+        `;
+
+        listaTarefas.appendChild(item);
+    });
+}
+listarTarefas();
+
+formularioTarefa.addEventListener("submit", async (event) => {
+    event.preventDefault();
+
+    const titulo = campoTitulo.value;
+    const descricao = campoDescricao.value;
+
+    const response = await fetch("/tarefas", {
+        method: "POST",
+        headers: {
+            "Content-Type": "application/json"
+        },
+        body: JSON.stringify({
+            titulo,
+            descricao
+        })
+    });
+
+    if (response.ok) {
+        campoTitulo.value = "";
+        campoDescricao.value = "";
+
+        await listarTarefas();
+    }
+});
